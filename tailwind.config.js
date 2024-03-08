5b456ba8e64eabe5f4921cd8965e61504851b398
deji-dd <dejib50@gmail.com>
2024-03-08 20:27:36 +0100

    added delay

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       animation: {
-        "infinite-scroll": "infinite-scroll 25s linear infinite",
+        "infinite-scroll": "infinite-scroll 25s 2s linear infinite",
       },
       keyframes: {
         "infinite-scroll": {
